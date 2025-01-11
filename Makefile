61d96d5e789c03f11be425e674446d0a04f96709
SUZUKI Kei <todokimasen@gmail.com>
2025-01-12 08:12:12 +0900

    テストコードを作成した.

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 
+.DEFAULT_GOAL := test
+
+.PHONY: test
+test:
+	@ruby -I. test_brainfuck.rb
+
 .PHONY: run
 run:
 	@ruby brainfuck.rb
---
+++ test_brainfuck.rb
@@ -0,0 +1,148 @@
+require 'stringio'
+require 'test/unit'
+require 'brainfuck'
+
+module Brainfuck
+
+    class TestCase < Test::Unit::TestCase
+
+        protected
+
+        def capture_stdout
+            original_stdout = $stdout
+            $stdout = StringIO.new
+            yield
+            $stdout.string
+        ensure
+            $stdout = original_stdout
+        end
+
+    end
+
+    class SpecificationTestCase < TestCase
+
+        class SpecificationIncluded
+            include Brainfuck::Specification
+        end
+
+        def test_COMMANDS
+            assert_equal 8, SpecificationIncluded::COMMANDS.size
+            assert_true SpecificationIncluded::COMMANDS.include?('+')
+            assert_true SpecificationIncluded::COMMANDS.include?('-')
+            assert_true SpecificationIncluded::COMMANDS.include?('>')
+            assert_true SpecificationIncluded::COMMANDS.include?('<')
+            assert_true SpecificationIncluded::COMMANDS.include?('[')
+            assert_true SpecificationIncluded::COMMANDS.include?(']')
+            assert_true SpecificationIncluded::COMMANDS.include?('.')
+            assert_true SpecificationIncluded::COMMANDS.include?(',')
+        end
+
+        def test_NUMBER_OF_CELLS
+            assert_equal 30000, SpecificationIncluded::NUMBER_OF_CELLS
+        end
+
+        def test_MIN_CELL_VALUE
+            assert_equal -128, SpecificationIncluded::MIN_CELL_VALUE
+        end
+
+        def test_MAX_CELL_VALUE
+            assert_equal 127, SpecificationIncluded::MAX_CELL_VALUE
+        end
+
+    end
+
+    class ParserTestCase < TestCase
+
+        def test_parse
+            source_code = <<~'EOS'
+                # H
+                ++++++++[->+++++++++<]>.
+                # e
+                <++++[->+++++++<]>+.
+                # l
+                +++++++.
+                # l
+                .
+                # o
+                +++.
+            EOS
+            expected = '++++++++[->+++++++++<]>.<++++[->+++++++<]>+.+++++++..+++.'
+            actual = Brainfuck::Parser.new.parse(source_code)
+            assert_equal expected, actual
+        end
+
+        [
+            'あ',
+            '>あ',
+            'あ<',
+            'テスト',
+            '>テ+ス+ト<',
+        ].each.with_index do |source_code, index|
+            define_method("test_parse_raises_ParseError_when_source_code_includes_non_ascii_character_#{index + 1}") do
+                assert_raises(Brainfuck::ParseError) do
+                    Brainfuck::Parser.new.parse(source_code)
+                end
+            end
+        end
+
+        [
+            '[',
+            ']',
+        ].each.with_index do |source_code, index|
+            define_method("test_parse_raises_SyntaxError_when_unmatched_brackets_exists_#{index + 1}") do
+                assert_raises(Brainfuck::SyntaxError) do
+                    Brainfuck::Parser.new.parse(source_code)
+                end
+            end
+        end
+
+    end
+
+    class VirtualMachineTestCase < TestCase
+
+        HELLO_WORLD = <<~'EOS'
+            ++++++++[->+++++++++<]>.
+            <++++[->+++++++<]>+.
+            +++++++.
+            .
+            +++.
+            >++++[->+++++++++++<]>.
+            ------------.
+            <+++++[->+++++++++++<]>.
+            <++++[->++++++<]>.
+            +++.
+            ------.
+            --------.
+            >++++[->++++++++<]>+.
+            >+++[->++++<]>+.
+            ---.
+        EOS
+
+        def test_cells
+            vm = Brainfuck::VirtualMachine.new
+            assert_equal [0] * 30000, vm.cells
+
+            stdout = capture_stdout { vm.execute(HELLO_WORLD) }
+            expected = [0, 111, 0, 100, 0, 33, 0, 10, *([0] * 30000)].take(30000)
+            assert_equal expected, vm.cells
+        end
+
+        def test_used_cells
+            vm = Brainfuck::VirtualMachine.new
+            assert_equal [0], vm.used_cells
+
+            stdout = capture_stdout { vm.execute(HELLO_WORLD) }
+            assert_equal [0, 111, 0, 100, 0, 33, 0, 10], vm.used_cells
+        end
+
+        def test_execute
+            vm = Brainfuck::VirtualMachine.new
+            stdout = capture_stdout { vm.execute(HELLO_WORLD) }
+            assert_equal "Hello, World!\r\n", stdout
+            assert_equal [0, 111, 0, 100, 0, 33, 0, 10], vm.used_cells
+        end
+
+    end
+
+end
+
