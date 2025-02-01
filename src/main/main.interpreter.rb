82f634b0cc662c3973ec67ae531cc52f24c4cafe
SUZUKI Kei <todokimasen@gmail.com>
2025-02-01 15:57:48 +0900

    想定するエラーの場合は backtrace を出力しないように変更した.

--- src/main/main.interpreter.rb
+++ src/main/main.interpreter.rb
@@ -1,9 +1,12 @@
+require 'brainfuck/errors'
 require 'brainfuck/virtual_machine'
 
 def main
     source_code = ARGF.read
     vm = Brainfuck::VirtualMachine.new
     vm.execute(source_code)
+rescue Brainfuck::BrainfuckError => error
+    $stderr.puts "#{error.class.name.split('::').last} - #{error.to_s}"
 rescue Interrupt
     # ignore
 end
