2d0e8379fc38da8e892e9c0fcf0fc863d9959905
SUZUKI Kei <todokimasen@gmail.com>
2025-02-04 21:20:51 +0900

    "make demonstrate-generators" が動かなくなっていた問題を修正した.

--- src/scripts/demonstrate-generators.sh
+++ src/scripts/demonstrate-generators.sh
@@ -7,7 +7,7 @@ declare -gr ROOT_DIR="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
 for i in $(seq 4)
 do
     echo "==== generator${i}"
-    echo -ne 'Hello, World!\r\n' | ${ROOT_DIR}/bin/brainfuck.generator${i}
+    echo -ne 'Hello, World!\r\n' | ${ROOT_DIR}/bin/brainfuck generate -${i}
     echo
 done
 
