0e72e47f7777cb17b4844172c5c7a4c9c51695cd
SUZUKI Kei <todokimasen@gmail.com>
2025-02-04 23:48:38 +0900

    シェルスクリプトのテストを追加した.

--- src/test/brainfuck/base.rb
+++ src/test/brainfuck/base.rb
@@ -5,6 +5,8 @@ module Brainfuck
 
     class TestCase < Test::Unit::TestCase
 
+        ROOT_DIR = File.absolute_path(File.join(File.dirname(__FILE__), '..', '..', '..'))
+
         def capture_stdin(stdin)
             original_stdin = $stdin
             $stdin = StringIO.new(stdin.clone, 'r')
---
+++ src/test/brainfuck/test_scripts.rb
@@ -0,0 +1,22 @@
+require 'open3'
+require_relative 'base'
+
+module Brainfuck
+
+    # ./src/scripts/ 以下のスクリプトのテスト.
+    # 意図せず起動しなくなることを検知できる程度の簡単な確認をおこなう.
+    class ScriptsTestCase < TestCase
+
+        SCRIPTS_DIR = File.join(ROOT_DIR, 'src', 'scripts')
+
+        def test_demonstrate_generators
+            Dir.chdir(SCRIPTS_DIR) do
+                stdout, stderr, status = Open3.capture3('bash demonstrate-generators.sh')
+                assert_equal 0, status.exitstatus
+            end
+        end
+
+    end
+
+end
+
