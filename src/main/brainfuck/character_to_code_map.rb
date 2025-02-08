f18f227eebac5fc1effc98abb2910dd36c3a0efd
SUZUKI Kei <todokimasen@gmail.com>
2025-02-08 21:36:27 +0900

    ループ外の + の数が少ないコードを優先するように変更した.

--- src/main/brainfuck/character_to_code_map.rb
+++ src/main/brainfuck/character_to_code_map.rb
@@ -30,16 +30,16 @@ module Brainfuck
 
                     (min_b .. max_b).each do |b|
                         code = Code.new(a, b, 0)
-                        shortest_code = map.fetch(code.n, code)
-                        map[code.n] = code if code.code_length <= shortest_code.code_length
+                        codes = [map[code.n], code].compact
+                        map[code.n] = codes.sort_by(&:code_length).first
 
                         min_c = MIN_CELL_VALUE - (a * b)
                         max_c = MAX_CELL_VALUE - (a * b)
 
                         (min_c .. max_c).each do |c|
                             code = Code.new(a, b, c)
-                            shortest_code = map.fetch(code.n, code)
-                            map[code.n] = code if code.code_length <= shortest_code.code_length
+                            codes = [map[code.n], code].compact
+                            map[code.n] = codes.sort_by(&:code_length).first
                         end
                     end
                 end
